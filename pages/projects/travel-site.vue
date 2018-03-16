af27ff5a264a6fc635fcd7d6cf129d6d17713bea
Vasileios Bakas <bill.bakas92@gmail.com>
2018-03-16 18:34:35 +0200

    WIP

--- pages/projects/travel-site.vue
+++ pages/projects/travel-site.vue
@@ -13,19 +13,23 @@
           This course introduced me to many industry standard technologies such as npm, GIT, ES6, Sass, PostCSS, Gulp and Node helping me grasp a better understanding of the modern web development workflow. 
           Up until now, i refer to this particular project because it breaks down the workflow of building a website in a nice and structured way. 
         </p>
+        <div class="description__cta">
+          <a class="btn btn--white" href="https://bill-mpakas.github.io/travel-site/">View Project</a>
+        </div>
       </div>
       <div class="tech">
           <h3 class="description__title">Technologies Used</h3>
           <div class="tech__logos">
-            <img src="~/assets/logos/html.svg" alt="">
-            <img src="~/assets/logos/html.svg" alt="">
-            <img src="~/assets/logos/html.svg" alt="">
-            <img src="~/assets/logos/html.svg" alt="">
-            <img src="~/assets/logos/html.svg" alt="">
+            <img src="~/assets/icons/html.svg" alt="html5">
+            <img src="~/assets/icons/css.svg" alt="html5">
+            <img src="~/assets/icons/javascript.svg" alt="html5">
+            <img src="~/assets/icons/gulp.svg" alt="html5">
+            <img src="~/assets/icons/npm.svg" alt="html5">
+            <img src="~/assets/icons/git.svg" alt="html5">
           </div>
       </div>
-      <div class="details product-image">
-        
+      <div class="details">
+        <img class="product-image" src="~assets/images/mockups/travel-site.jpg" alt="">
       </div>
     </div>
   </div>
@@ -39,7 +43,7 @@
   background: #FAFAFA;
   box-shadow: 15px 15px 20px lightgray;
   display: grid;
-  grid-template-rows: auto 2fr 1fr;
+  grid-template-rows: 1fr auto 1fr;
   grid-template-columns: repeat(auto-fill, minmax(250px, 50%));
   grid-template-areas:
   "title  details"
@@ -53,7 +57,7 @@
   background: #FAFAFA;
   box-shadow: 15px 15px 20px lightgray;
   display: grid;
-  grid-template-rows: auto 2fr 1fr 1fr;
+  grid-template-rows: auto auto auto auto;
   grid-template-columns: 100%;
   grid-template-areas:
   "title"
@@ -89,7 +93,14 @@
     padding-left: 20px;
     padding-right: 20px;
     text-align: left;
-    
+  }
+
+  &__cta {
+
+    @media (max-width: 900px) {
+      padding-top: 0;
+    }
+    padding-top: 80px;
   }
 }
 
@@ -97,10 +108,10 @@
   grid-area: tech;
 
   &__logos {
-    padding: 20px;
+    
     display: grid;
     grid-gap: 40px;
-    grid-template-columns: repeat(auto-fit, minmax(20px,50px));
+    grid-template-columns: repeat(auto-fit, minmax(20px,35px));
     justify-content: center;
   }
 }
@@ -108,12 +119,6 @@
 .details {
   grid-area: details;
   background-color: yellow;
-}
-
-.product-image {
-  background: url('~/assets/images/card.jpg');
-  background-size: cover;
-  background-position: 50%;
   
 }
 
