3529a0d803b7702ecdfb92fd6547ca2de7f1d57b
Vasileios Bakas <bill.bakas92@gmail.com>
2018-03-19 13:33:52 +0200

    About page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,81 +1,206 @@
 <template>
-  <div class="wrapper-box">
     <div class="main-content">
-      <div class="avatar">
-        <img src="http://i.pravatar.cc/170" alt="" class="avatar__img">
-        <div class="avatar__social">fb, linkedin</div>
+      <div class="left-content">
+          <div class="avatar">
+            <img src="~/assets/images/profile_pic.png" alt="" class="avatar__img">
+            <div class="avatar__social">
+              <svg class="icon icon-instagram"><use xlink:href="#icon-instagram"></use></svg>
+              <symbol id="icon-instagram" viewBox="0 0 32 32">
+<title>instagram</title>
+<path d="M16 2.881c4.275 0 4.781 0.019 6.462 0.094 1.563 0.069 2.406 0.331 2.969 0.55 0.744 0.288 1.281 0.638 1.837 1.194 0.563 0.563 0.906 1.094 1.2 1.838 0.219 0.563 0.481 1.412 0.55 2.969 0.075 1.688 0.094 2.194 0.094 6.463s-0.019 4.781-0.094 6.463c-0.069 1.563-0.331 2.406-0.55 2.969-0.288 0.744-0.637 1.281-1.194 1.837-0.563 0.563-1.094 0.906-1.837 1.2-0.563 0.219-1.413 0.481-2.969 0.55-1.688 0.075-2.194 0.094-6.463 0.094s-4.781-0.019-6.463-0.094c-1.563-0.069-2.406-0.331-2.969-0.55-0.744-0.288-1.281-0.637-1.838-1.194-0.563-0.563-0.906-1.094-1.2-1.837-0.219-0.563-0.481-1.413-0.55-2.969-0.075-1.688-0.094-2.194-0.094-6.463s0.019-4.781 0.094-6.463c0.069-1.563 0.331-2.406 0.55-2.969 0.288-0.744 0.638-1.281 1.194-1.838 0.563-0.563 1.094-0.906 1.838-1.2 0.563-0.219 1.412-0.481 2.969-0.55 1.681-0.075 2.188-0.094 6.463-0.094zM16 0c-4.344 0-4.887 0.019-6.594 0.094-1.7 0.075-2.869 0.35-3.881 0.744-1.056 0.412-1.95 0.956-2.837 1.85-0.894 0.888-1.438 1.781-1.85 2.831-0.394 1.019-0.669 2.181-0.744 3.881-0.075 1.713-0.094 2.256-0.094 6.6s0.019 4.887 0.094 6.594c0.075 1.7 0.35 2.869 0.744 3.881 0.413 1.056 0.956 1.95 1.85 2.837 0.887 0.887 1.781 1.438 2.831 1.844 1.019 0.394 2.181 0.669 3.881 0.744 1.706 0.075 2.25 0.094 6.594 0.094s4.888-0.019 6.594-0.094c1.7-0.075 2.869-0.35 3.881-0.744 1.050-0.406 1.944-0.956 2.831-1.844s1.438-1.781 1.844-2.831c0.394-1.019 0.669-2.181 0.744-3.881 0.075-1.706 0.094-2.25 0.094-6.594s-0.019-4.887-0.094-6.594c-0.075-1.7-0.35-2.869-0.744-3.881-0.394-1.063-0.938-1.956-1.831-2.844-0.887-0.887-1.781-1.438-2.831-1.844-1.019-0.394-2.181-0.669-3.881-0.744-1.712-0.081-2.256-0.1-6.6-0.1v0z"></path>
+<path d="M16 7.781c-4.537 0-8.219 3.681-8.219 8.219s3.681 8.219 8.219 8.219 8.219-3.681 8.219-8.219c0-4.537-3.681-8.219-8.219-8.219zM16 21.331c-2.944 0-5.331-2.387-5.331-5.331s2.387-5.331 5.331-5.331c2.944 0 5.331 2.387 5.331 5.331s-2.387 5.331-5.331 5.331z"></path>
+<path d="M26.462 7.456c0 1.060-0.859 1.919-1.919 1.919s-1.919-0.859-1.919-1.919c0-1.060 0.859-1.919 1.919-1.919s1.919 0.859 1.919 1.919z"></path>
+</symbol>
+<svg class="icon icon-facebook2"><use xlink:href="#icon-facebook2"></use></svg>
+<symbol id="icon-facebook2" viewBox="0 0 32 32">
+<title>facebook2</title>
+<path d="M29 0h-26c-1.65 0-3 1.35-3 3v26c0 1.65 1.35 3 3 3h13v-14h-4v-4h4v-2c0-3.306 2.694-6 6-6h4v4h-4c-1.1 0-2 0.9-2 2v2h6l-1 4h-5v14h9c1.65 0 3-1.35 3-3v-26c0-1.65-1.35-3-3-3z"></path>
+</symbol>
+<svg class="icon icon-linkedin"><use xlink:href="#icon-linkedin"></use></svg>
+<symbol id="icon-linkedin" viewBox="0 0 32 32">
+<title>linkedin</title>
+<path d="M29 0h-26c-1.65 0-3 1.35-3 3v26c0 1.65 1.35 3 3 3h26c1.65 0 3-1.35 3-3v-26c0-1.65-1.35-3-3-3zM12 26h-4v-14h4v14zM10 10c-1.106 0-2-0.894-2-2s0.894-2 2-2c1.106 0 2 0.894 2 2s-0.894 2-2 2zM26 26h-4v-8c0-1.106-0.894-2-2-2s-2 0.894-2 2v8h-4v-14h4v2.481c0.825-1.131 2.087-2.481 3.5-2.481 2.488 0 4.5 2.238 4.5 5v9z"></path>
+</symbol>
+              </div>
+          </div>
+          <div class="bio">
+          <h3 class="bio__intro">My Journey</h3>
+          <p class="bio__main">
+            When I was young I had this dream about becoming a pro skateboarder. 
+            I was the first to be at the skate park every day and the last to leave. 
+            I would spend every day pushing myself to become better and better. 
+            Sadly, I didn’t manage to become a pro skateboarder, but what I did learn from skateboarding is the consistent work it takes to be good at something – knowledge I apply to the professional work I do now.
+          </p>
+          </div>
+          <div class="cta">
+            <a class="btn btn--blue" href="">Download CV</a>
+            <a class="btn btn--blue" href="">View Projects</a>
+          </div>
       </div>
-      <div class="content">
-        <h3 class="content__intro">My Journey</h3>
-        <p class="content__main">
-          When I was young I had this dream about becoming a pro skateboarder. 
-          I was the first to be at the skate park every day and the last to leave. 
-          knowledge I apply to the professional work I do now.
-        </p>
-      </div>
-      <a class="btn btn--blue" href="">Download CV</a>
-      <div class="details img2">
-        <img src="~/assets/images/home.jpg" alt="">
+      <div class="right-content">
+         <div class="skills">
+           <h2 class="skills_title">What I do</h2>
+         </div>
+         <div class="skills-card">
+           <div class="card">
+              <span class="card__icon">
+                <img src="~/assets/icons/design.svg" height="60px" alt="">
+              </span>
+              <h3 class="card__title">Design</h3>
+              <p class="card__description">I love to design layouts for the web.</p>
+            </div>
+            <div class="card">
+              <span class="card__icon">
+                <img src="~/assets/icons/idea.svg" height="60px" alt="">
+              </span>
+              <h3 class="card__title">Web Development</h3>
+              <p class="card__description">Getting these designs into a working interface, excites me.</p>
+            </div>
+            <div class="card">
+              <span class="card__icon">
+                <img src="~/assets/icons/wordpress-skill.svg" alt="" height="60px">
+              </span>
+              <h3 class="card__title">Wordpress</h3>
+              <p class="card__description">Wordpress powers up the 30% of the web. <br> I also love it</p>
+            </div>
+            <div class="card">
+              <span class="card__icon">
+                <img src="~/assets/icons/search.svg" alt="" height="60px">
+              </span>
+              <h3 class="card__title">Digital Marketing</h3>
+              <p class="card__description">Reach new customers using smart, digital strategies.</p>
+            </div>
+         </div>
       </div>
     </div>
-  </div>
 </template>
 
 <style lang="scss" scoped>
   $page_color: #625B7E;
 
-  .main-content {
-  grid-area: iface;
-  background: #FAFAFA;
-  box-shadow: 15px 15px 20px lightgray;
-  display: grid;
-  grid-template-columns: 50% auto;
-  grid-template-areas:
-  "avatar  details"
-  "content details"
-  "cv details"
+  .icon {
+  display: inline-block;
+  width: 2em;
+  height: 2em;
+  stroke-width: 0;
+  stroke: $page_color;
+  fill: $page_color;
 }
 
-.avatar {
-  grid-area: avatar;
-  display: grid;            /* new */
-  align-items: center;      /* new */
-  justify-items: center;   
+  .main-content {
+    display: grid;
+    grid-template-columns: 50% 50%;
 
-  &__img {
-    align-content: center;
-    align-items: center;
+    @media screen and (max-width: 700px) {
+        display: block;
+      }
+
+  }
+
+  .left-content {
+    display: grid;
+    grid-template-rows: auto 50% auto;
+    grid-template-areas:
+    "avatar"
+    "bio"
+    "cta"
+  }
+
+  .avatar {
+    grid-area: avatar;
+    display: grid;
+    align-items: center;      /* new */
+    justify-items: center;
+    
+    &__img {
+    height: 150px;
     border-radius: 50%;
   }
 
-  &__social {
-    text-align: center;
-  }
-}
+    &__social {
+      text-align: center;
 
-.content {
-  grid-area: content;
-
-  &__intro {
-    text-align: center;
-    font-size: 1rem;
+      svg {
+        padding-right: 10px;
+      }
+    }
   }
-  &__main {
-    font-size: 1rem;
+
+  .bio {
+    grid-area: bio;
+
+    &__intro {
+    text-align: center;
+    font-size: 1.3rem;
+    }
+    &__main {
+    line-height: 1.8;
+    text-align: left;
+    font-size: 1.1rem;
     padding: 0 30px 0 30px;
-  }
-}
-.details {
-  background: $page_color;
+    }
 
-  img {
-    width: 100%;
   }
-}
 
-.btn {
-  grid-area: cv;
-}
+  .cta {
+    grid-area: cta;
+    justify-self: center;
+    align-self: center;
+
+    a {
+      margin-right: 10px
+    }
+  }
+
+  // Right Content
+
+  .right-content {
+    background-color: #FAFAFA;
+    display: grid;
+    grid-template-rows: auto 1fr;
+  }
+
+  .skills {
+    align-self: center;
+    justify-self: center;
+  }
+
+  .skills-card {
+    display: grid;
+    padding-left: 10px;
+    padding-right: 10px;
+    grid-gap: 20px;
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
+    align-self: center;
+    
+  }
+
+  .card {
+    background: white;
+    border: 1px solid white;
+    text-align: center;
+    padding-top: 10px;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.1);
+
+    &__icon {
+      background: url('~/assets/icons/wordpress.svg');
+    }
+
+    &__title {
+
+    }
+
+    &__description {
+      padding: 10px;
+    }
+  }
+
+
+
+
+
+
 </style>
 
 
