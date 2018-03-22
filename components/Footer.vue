c8e1780628b211d20b13761391795fcdc265bf00
Vasileios Bakas <bill.bakas92@gmail.com>
2018-03-22 20:52:56 +0200

    Header Footer WIP

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,34 +1,15 @@
 <template>
-		<nav class="menu">
-       <ul class="menu-items">
-        <li><nuxt-link to="/">Home</nuxt-link></li>
-        <li><nuxt-link to="/about">About</nuxt-link></li>
-        <li><nuxt-link to="/projects">Projects</nuxt-link></li>
-        <li><nuxt-link to="/contact">Contact</nuxt-link></li>
+      <ul class="footer">
+        <p>Made with Love in Thessaloniki</p>
       </ul>
-    </nav>
 </template>
 
 <style lang="scss" scoped>
-  .menu ul {
-    display: grid;
-    align-items: center;      /* new */
-    justify-items: center;
-    list-style: none;
-    grid-gap: 20px;
-    grid-auto-columns: auto;
-    grid-template-columns: repeat(auto-fill, minmax(40px, 1fr));
-    a {
-      display: block;
-      text-decoration: none;
-      text-align: center;
-      color: black;
-      text-transform:lowercase;
-      font-size: 14px;
-    }
 
-    li {
-      
-    }
+  .footer {
+    grid-area: footer;
+  }
+  p {
+    text-align: center;
   }
 </style>
