d1a960791d5fcbd6a2af45153752d82e576a5a81
Vasileios Bakas <bill.bakas92@gmail.com>
2018-03-22 12:46:34 +0200

    Updated about

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -26,10 +26,9 @@
           <div class="bio">
           <h3 class="bio__intro">About me</h3>
           <p class="bio__main">
-            When I was young I had this dream about becoming a pro skateboarder. 
-            I was the first to be at the skate park every day and the last to leave. 
-            I would spend every day pushing myself to become better and better. 
-            Sadly, I didn’t manage to become a pro skateboarder, but what I did learn from skateboarding is the consistent work it takes to be good at something – knowledge I apply to the professional work I do now.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </p>
           </div>
           <div class="cta">
